8f413183a08c5600a4e1c592f8e1578538d58bdb
Nishant Patil <ncp1435@gmail.com>
2025-12-06 15:14:02 +0530

    Updated URL

--- src/InvoiceViewer.jsx
+++ src/InvoiceViewer.jsx
@@ -8,7 +8,7 @@ export default function InvoiceViewer() {
   useEffect(() => {
     console.log('here');
 
-    fetch('https://thinkbridgeinvoiceapi.azurewebsites.net/api/invoices')
+    fetch('https://thinkbridgeinvoiceapi.azurewebsites.net/api/invoice')
       .then((resp) => {
         if (!resp.ok) throw new Error('Network response was not ok');
         return resp.json();
